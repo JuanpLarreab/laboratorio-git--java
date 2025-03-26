28c91e14ff2af304dddfc8b20169549ce8e47982
Elisabet <elisabet.arcilam@uqvirtual.edu.co>
2025-03-25 20:48:08 -0500

    añadida MiClase

---
+++ src/com/laboratorio/curso.java
@@ -0,0 +1,42 @@
+package Git;
+
+public class curso {
+   private string nombre;
+   private int id;
+   private double puntaje;
+   
+}
+public curso(string nombre; int id; double puntaje) {
+	
+	this.nombre = nombre;
+	this.id = id;
+	this.puntaje = puntaje;
+	
+}
+public String getNombre() {
+	return nombre;
+	
+}
+public int getId() {
+	return id;
+	
+}
+public double getPuntaje() {
+	return puntaje;
+
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public void setPuntaje(double puntaje) {
+	this.puntaje = puntaje;
+	
+}
+
+}
+
